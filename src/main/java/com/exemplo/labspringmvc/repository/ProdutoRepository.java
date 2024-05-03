5375a5919029d5637e26087c8cae0c4171d4f397
Breno <brenorosaalmeida@yahoo.com.br>
2024-05-03 18:56:58 -0300

    Exclusão das classes do BD

--- src/main/java/com/exemplo/labspringmvc/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exemplo.labspringmvc.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.exemplo.labspringmvc.models.Produto;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
-    
-}
