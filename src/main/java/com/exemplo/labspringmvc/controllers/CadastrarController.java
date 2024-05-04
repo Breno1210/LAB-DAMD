96b57b7f729cb342a6fce293d40ac90dc286b18d
Breno <brenorosaalmeida@yahoo.com.br>
2024-05-03 21:14:43 -0300

    Adicionado páginas de Home, Cadastro e Login.

---
+++ src/main/java/com/exemplo/labspringmvc/controllers/CadastrarController.java
@@ -0,0 +1,14 @@
+package com.exemplo.labspringmvc.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CadastrarController {
+
+    @GetMapping("/cadastrar")
+    public String cadastro() {
+        return "cadastrar";
+    }
+}
--- src/main/java/com/exemplo/labspringmvc/controllers/HomeController.java
+++ src/main/java/com/exemplo/labspringmvc/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
-public class HelloWorld {
+public class HomeController {
     
     @GetMapping("/")
     public String index (Model model) {
